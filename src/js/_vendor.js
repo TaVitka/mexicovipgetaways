feb5e8f7fc0fdf31d234169d6c806cf994ac6c27
TaVitka <tavitka17@gmail.com>
2024-05-13 17:17:06 +0300

    added project files, added logo and favicon

---
+++ src/js/_vendor.js
@@ -0,0 +1,8 @@
+// import $ from 'jquery';
+// import LottiePlayer from '@lottiefiles/lottie-player';
+// import slick from 'slick-slider';
+// import './vendor/_focus-visible';
+
+// window.$ = $;
+// window.slick = slick || {};
+// window.LottiePlayer = LottiePlayer;
